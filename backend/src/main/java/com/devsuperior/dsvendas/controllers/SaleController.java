f1a6526ef14f8cb70384fb8c57ee5d4bd758fac2
VictorN7 <victorhnogueira@hotmail.com>
2021-09-09 14:01:53 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dsvendas.controllers;
 
-
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,8 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.service.SaleService;
 
 
@@ -26,6 +27,16 @@ public class SaleController {
 		Page<SaleDTO> page = service.finAll(pageable);
 		return ResponseEntity.ok(page);
 		
+	}
+	@GetMapping(value ="/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySellers(){
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	@GetMapping(value ="/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySellers(){
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
+		return ResponseEntity.ok(list);
 	}
 	
 }
--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -1,11 +1,23 @@
 package com.devsuperior.dsvendas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 
 //Essa classe não precisa de anotation componente e nenhum outro, por padrao o JPARepository já é um componente
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	// obj.seller é o objeto seller da classe Sale.java
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount))"
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
 	
-	
+	@Query("SELECT new com.devsuperior.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals))"
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
 }
