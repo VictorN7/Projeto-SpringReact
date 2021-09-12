cdeaf47c9428741723cc589e8ab55ac7e4935d4c
VictorN7 <victorhnogueira@hotmail.com>
2021-09-12 15:54:02 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8080';  // 
-
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';  // '??' Operador de coalescência nula
+//Obtem a referencia da variavel onde a aplicação estiver rodando  
 
 
