370f068c0410a1fd27f4982a592ff39b8f9261db
VictorN7 <victorhnogueira@hotmail.com>
2021-09-12 13:15:01 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,57 @@
 import Chart from 'react-apexcharts';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { SaleSuccess } from '../../types/sale';
+import { BASE_URL } from '../../utils/requests';
+import { round } from '../../utils/format';
+
+type SeriesData = {
+    name :string;
+    data: number[];
+}
+type ChartData = {
+    labels: {
+        categories : string[];
+
+    };
+    series : SeriesData[];
+}
 
 const BarChart = () => {
+
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []                   
+            }
+        ]
+    });
+
+    useEffect(() => { 
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSuccess[]; // Casting da List de Collection da api para o SaleSum do react 
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));
+
+            setChartData({
+                labels: {
+                    categories: myLabels
+                },
+                series: [
+                    {
+                        name: "% de Sucesso",
+                        data: mySeries                    
+                    }
+                ]
+            });
+        })
+    },[])
+
     const options = {
         plotOptions: {
             bar: {
@@ -9,7 +60,7 @@ const BarChart = () => {
         },
     };
     
-    const mockData = {
+    /* const mockData = {
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
         },
@@ -19,12 +70,12 @@ const BarChart = () => {
                 data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
             }
         ]
-    };
+    }; */
     return (
         /* o '...options' pega as config da variavel do options e reaproveita ele no chart, e tambem podendo incluir mais coisas nessa parte...  */
        <Chart 
-            options={{...options,xaxis:mockData.labels}} // xaxis pega o labels do mockdata, colocando então as informações contidas nele.
-            series={mockData.series}  
+            options={{...options,xaxis:chartData.labels}} // xaxis pega o labels do mockdata, colocando então as informações contidas nele.
+            series={chartData.series}  
             type="bar" 
             height="240"
        />
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -25,7 +25,7 @@ const DonutChart = () => {
 
             setChartData({ labels:myLabels, series: mySeries});
         })
-    },[])
+    },[]);
     
     /* const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
