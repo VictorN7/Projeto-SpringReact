4d7ab22cc6a8d9c26003c58a02ddbe4231b8b95c
VictorN7 <victorhnogueira@hotmail.com>
2021-09-12 03:57:47 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,11 +1,35 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from '../../types/sale';
+import { BASE_URL } from '../../utils/requests';
+
+
+type CharData = {
+    labels:string[];
+    series:number[];
+
+}
+
 const DonutChart = () => {
 
-    const mockData = {
+    // Forma errada
+    let chartData :  CharData = { labels:[], series:[]};
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSum[]; // Casting da List de Collection da api para o SaleSum do react 
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = { labels:myLabels, series: mySeries};
+
+            console.log(chartData);
+        })
+    /* const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
-    
+    } */
+
     const options = {
         legend: {
             show: true
@@ -13,12 +37,12 @@ const DonutChart = () => {
     }
     return (
         /* o '...options' pega as config da variavel do options e reaproveita ele no chart, e tambem podendo incluir mais coisas nessa parte...  */
-       <Chart 
-            options={{...options,labels:mockData.labels}} // xaxis pega o labels do mockdata, colocando então as informações contidas nele.
-            series={mockData.series}  
-            type="donut" 
+        <Chart
+            options={{ ...options, labels: chartData.labels }} // xaxis pega o labels do mockdata, colocando então as informações contidas nele.
+            series={chartData.series}
+            type="donut"
             height="240"
-       />
+        />
     );
 }
 
